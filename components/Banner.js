6034b09ba410e93c283f33812cd46799d7f18139
Pannaga <pannaga1123@gmail.com>
2022-11-17 12:43:46 +0530

    refactor Image

--- components/Banner.js
+++ components/Banner.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px]" >
-            <Image className='' src="https://links.papareact.com/0fm" layout='fill' objectFit='cover' alt="" />
+            <Image src="https://links.papareact.com/0fm" layout='fill' objectFit='cover' alt="" />
             <div className='absolute top-1/2 w-full text-center' >
                 <p>Not sure where to go? Perfect.</p>
-                <button className='text-red-500 bg-white border-2 border-red-500 px-10 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl hover:text-white hover:bg-red-500 active:scale-90 transition duration-200' >I'm Flexible</button>
+                <button className="text-red-500 bg-white border-2 border-red-500 px-10 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl hover:text-white hover:bg-red-500 active:scale-90 transition duration-200" >I'm Flexible</button>
             </div>
         </div>
     )
