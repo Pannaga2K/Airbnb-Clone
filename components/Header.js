6e7f7ff0efdd2f334677dff8b7915b068493f577
Pannaga <pannaga1123@gmail.com>
2022-11-17 17:12:44 +0530

    add calendar

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,29 @@ import { GlobeAltIcon } from "@heroicons/react/solid"
 import { UserCircleIcon } from "@heroicons/react/solid"
 import { UsersIcon } from "@heroicons/react/solid"
 import { MenuIcon } from "@heroicons/react/solid"
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection"
+    }
+
+    const handleSelect = (ranges) => {
+        console.log(ranges)
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-[#0f0f0f] text-white shadow-md py-5 px-5 md:px-10" >
             {/* LEFT */}
@@ -14,7 +35,7 @@ function Header() {
             </div>
             {/* MIDDLE */}
             <div className="flex items-center md:border-2 border-red-400 rounded-full py-2 md:shadow-sm" >
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-grey-400" type="text" placeholder='Start Yout Search' />
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-white placeholder-grey-400" type="text" placeholder='Start Yout Search' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-white text-black rounded-full p-2 cursor-pointer md:mx-2 " />
             </div>
             {/* RIGHT */}
@@ -26,6 +47,22 @@ function Header() {
                     <UserCircleIcon className='h-6' />
                 </div>
             </div>
+
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto transition duration-200" >
+                    <DateRangePicker className="text-black" ranges={[selectionRange]} minDate={new Date()} rangeColors={["#FD5B61"]} onChange={handleSelect} />
+                    <div className='flex items-center border-b my-4' >
+                        <h2 className='text-2xl flex-grow font-semibold' >Number of Guests</h2>
+                        <UsersIcon className='h-5' />
+                        <input className='w-12 pl-2 text-lg outline-none text-red-400 bg-transparent' type="number" min={1} value={noOfGuests} onChange={e => setNoOfGuests(e.target.value)} />
+                    </div>
+                    <div className='flex' >
+                        <button className='flex-grow text-gray-500' >Cancel</button>
+                        <button className='flex-grow text-red-400' >Search</button>
+                    </div>
+                </div>
+            )}
+             
         </header>
     )
 }
